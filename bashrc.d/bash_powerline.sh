966beed8ea3779bd6d6dae7a7d58acac48c9db87
Nate Marks <npmarks@gmail.com>
2022-04-25 07:02:54 -0400

    better bash/powerline.sh location

--- bashrc.d/bash_powerline.sh
+++ bashrc.d/bash_powerline.sh
@@ -7,6 +7,9 @@ POWERLINE_BASH_CONTINUATION=1
 POWERLINE_BASH_SELECT=1
 
 # locate the powerline bash source file with this
-# find ~ -type f -name powerline.sh | grep 'bash/powerline.sh'
-# shellcheck disable=SC1091
-. "${HOME}/.local/lib/python3.9/site-packages/powerline/bindings/bash/powerline.sh"
+# find $(python -m site --user-site) -type f -name powerline.sh | grep bash/powerline.sh
+# should return something like:
+#     /home/nate/.local/lib/python3.9/site-packages/powerline/bindings/bash/powerline.sh
+
+# shellcheck disable=SC1090
+. "$(find "$(python -m site --user-site)" -type f -name powerline.sh | grep bash/powerline.sh)"
