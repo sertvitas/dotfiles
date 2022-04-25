fa19ac8e6528720e18cb3b8bb8e7bcfad39fe765
Nate Marks <npmarks@gmail.com>
2022-04-25 15:11:54 -0400

    make fzf work for mac and linux

--- bashrc.d/fzf.sh
+++ bashrc.d/fzf.sh
@@ -1,10 +1,29 @@
 #!/usr/bin/env bash
+OS="$(uname -s)"
+function  fzf_completion() {
+  if [ "$OS" = "Darwin" ]; then
+    find /usr/local/Cellar/fzf -type f -name completion.bash
+  else
+    find /usr/share/doc/fzf -type f -name completion.bash
+  fi
+}
+
+
+function  fzf_key-bindings() {
+  if [ "$OS" = "Darwin" ]; then
+    find /usr/local/Cellar/fzf -type f -name key-bindings.bash
+  else
+    find /usr/share/doc/fzf -type f -name key-bindings.bash
+  fi
+}
 # Auto-completion
 # ---------------
 # shellcheck disable=SC1091
-[[ $- == *i* ]] && . "/usr/share/doc/fzf/examples/completion.bash" 2> /dev/null
+# $- == *i*  checks to see that the shell is interactive
+# shellcheck disable=SC1090
+[[ $- == *i* ]] && . "$(fzf_completion)" 2> /dev/null
 
 # Key bindings
 # ------------
-# shellcheck disable=SC1091
-. "/usr/share/doc/fzf/examples/key-bindings.bash"
+# shellcheck disable=SC1090
+. "$(fzf_key-bindings)"
