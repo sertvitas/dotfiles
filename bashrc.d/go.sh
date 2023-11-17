7407d61c84e11eb536a4fae98b0729e2cdb469d1
emoon <sertvitas@gmail.com>
2023-11-17 10:25:02 -0500

    Updates for gooooooooooooooooo

--- bashrc.d/go.sh
+++ bashrc.d/go.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-export PATH=$PATH:~/bin/go/1.18.1/bin:~/go/bin
+export PATH=$PATH:~/bin/go/1.21.3/go/bin:~/go/bin
---
+++ scripts/install_go.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+set -Eeuo pipefail
+
+GO_VERSION="1.21.3"
+
+mkdir -p ~/bin/go/"${GO_VERSION}"
+curl -L  "https://go.dev/dl/go${GO_VERSION}.linux-amd64.tar.gz" \
+--output ~/bin/go/${GO_VERSION}/go${GO_VERSION}.linux-amd64.tar.gz
+
+tar -xzvf ~/bin/go/${GO_VERSION}/go${GO_VERSION}.linux-amd64.tar.gz \
+--directory ~/bin/go/"${GO_VERSION}"
+
+go install -v golang.org/x/tools/gopls@latest
+go install honnef.co/go/tools/cmd/staticcheck@latest
+go install github.com/go-delve/delve/cmd/dlv@latest
